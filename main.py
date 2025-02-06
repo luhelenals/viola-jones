306b513957085f28001d099b3757db5af14e26d0
Luiza <luizahelena.lsantos@gmail.com>
2025-02-06 17:22:52 -0300

    refactoring + images

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import cv2
 import matplotlib.pyplot as plt
 
-# Carregar o classificador pré-treinado (pode ser treinado manualmente também)
-cascade = cv2.CascadeClassifier('C:/Users/luiza/source/repos/Viola Jones/cars.xml')
+def main():
+    file = input('Nome do arquivo: ')
+    cascade, cinza, imagem = load_image(file)
+    draw_vehicles(imagem, cinza, cascade)
+    show_image(imagem)
 
 # Carregar imagem de teste
-imagem = cv2.imread('C:/Users/luiza/source/repos/Viola Jones/cars.jpg')
-cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+def load_image(file):
+    # Carregar o classificador pré-treinado
+    cascade = cv2.CascadeClassifier('cars.xml')
 
-# Detectar veículos
-veiculos = cascade.detectMultiScale(
-    cinza,
-    scaleFactor=1.05,    # Tenta detectar melhor objetos pequenos
-    minNeighbors=3,      # Aceita menos vizinhos para validar um objeto
-    minSize=(50, 50),    # Ajuste para detectar carros menores
-    maxSize=(400, 400)   # Ajuste para evitar detectar objetos muito grandes
-)
+    imagem = cv2.imread(file)
+    cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-# Desenhar retângulos nos veículos detectados
-for (x, y, w, h) in veiculos:
-    cv2.rectangle(imagem, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    return cascade, cinza, imagem
+
+def draw_vehicles(src, cinza, cascade):
+    # Detectar veículos
+    veiculos = cascade.detectMultiScale(
+        cinza,
+        scaleFactor=1.05, 
+        minNeighbors=3,   
+        minSize=(50, 50), 
+        maxSize=(400, 400)
+    )
+
+    # Desenhar retângulos nos veículos detectados
+    for (x, y, w, h) in veiculos:
+        cv2.rectangle(src, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 # Exibir resultado
-plt.figure(figsize=(5, 5))
-plt.subplot(1, 1, 1)
-plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
-plt.axis("off")
-plt.show()
+def show_image(src):
+    plt.figure(figsize=(5, 5))
+    plt.subplot(1, 1, 1)
+    plt.imshow(cv2.cvtColor(src, cv2.COLOR_BGR2RGB))
+    plt.axis("off")
+    plt.show()
+
+if __name__ == '__main__':
+    main()
